371a56a51921a721123d0b0e78c41bb6f6dca4b0
LORDBABUINO <jorge.x7@gmail.com>
2019-11-10 09:42:02 -0300

    Controls component life cicle using react hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Container } from './styles'
 
 export default function Home() {
-  const [techs, setTechs] = useState(['ReactJS', 'React Native'])
+  const [techs, setTechs] = useState([])
   const [newTech, setNewTech] = useState([''])
   const handleAdd = () => {
     setTechs([...techs, newTech])
     setNewTech('')
   }
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs')
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs))
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs))
+  }, [techs])
 
   return (
     <>
