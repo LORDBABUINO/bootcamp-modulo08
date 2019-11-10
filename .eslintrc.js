98dd4f891bf75f561a4152541ca4e2645f357c7d
LORDBABUINO <jorge.x7@gmail.com>
2019-11-10 08:49:02 -0300

    Adds react hooks rules on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,7 @@ module.exports = {
   plugins: [
     'react',
     'prettier',
+    'react-hooks',
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -32,6 +33,11 @@ module.exports = {
     ],
     'react/state-in-constructor': ['warn', 'never'],
     'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
     semi: ['error', 'never'],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
