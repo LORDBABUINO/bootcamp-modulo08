58ce481e4d824c390c0bee6426c48e361ab14337
LORDBABUINO <jorge.x7@gmail.com>
2019-11-10 09:28:45 -0300

    Creates list using react hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Container } from './styles'
 
 export default function Home() {
+  const [techs, setTechs] = useState(['ReactJS', 'React Native'])
+  const [newTech, setNewTech] = useState([''])
+  const handleAdd = () => {
+    setTechs([...techs, newTech])
+    setNewTech('')
+  }
+
   return (
-		<Container>
-		</Container>
+    <>
+      <Container>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </Container>
+      <input
+        type="text"
+        onChange={({ target: { value } }) => setNewTech(value)}
+        value={newTech}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
   )
 }
